9843fcc6d7884df3c0b944c7089c75d82cb636ea
qyoyo1 <qyoyo1@hotmail.com>
2026-05-31 13:07:10 +0200

    Initial Commit

---
+++ FIREios/FIREiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  FIREiosApp.swift
+//  FIREios
+//
+//  Created by Yuying QIU on 31.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct FIREiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
